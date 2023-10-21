ed14d010d75bba8e9a8c258e6c943c0ca7927848
Tomatik Nikita <niktomas19@gmail.com>
2023-10-21 13:48:21 +0300

    123

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,34 @@
 #include <Arduino.h>
+#include <FastLED.h>
+
+#define DEBUG
+
+#define LED_PIN 2
+#define LED_COUNT 64
+
+CRGB stick[LED_COUNT];
 
 void setup() {
-    // put your setup code here, to run once:
+#ifdef DEBUG
+    Serial.begin(9600);
+    Serial.println("Start");
+#endif
+    FastLED.addLeds<WS2812B, LED_PIN, RGB>(stick, LED_COUNT);
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+
+    for (size_t i = 0; i < 2; i++) {
+#ifdef DEBUG
+        Serial.println(i);
+#endif
+        stick[i].setColorCode(0x101010);
+        FastLED.show();
+        delay(500);
+    }
+    for (size_t i = 0; i < 2; i++) {
+        stick[i] = CRGB::Black;
+    }
+    FastLED.show();
+    delay(500);
 }
